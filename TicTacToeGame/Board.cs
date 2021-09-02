26c8caf59acacc4ee474ac314f2d078156c7ba06
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-02 12:33:04 +0530

    feat:Initializing Board With Empty Value

---
+++ TicTacToeGame/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class Board
+    {
+        public Board()
+        {
+            char[] board = InitializeBoard();
+        }
+
+        //Intializing Board with Empty Value
+        public char[] InitializeBoard()
+        {
+            //Char Array of 10 elements
+            char[] board = new char[10];
+
+            //Assigning Space:-Index 1 to 9
+            for (int index = 1; index < board.Length; index++)
+            {
+                board[index] = ' ';
+            }
+            return board;
+        }
+    }
+}
